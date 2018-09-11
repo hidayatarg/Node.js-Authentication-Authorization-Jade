4296c7e9aa9653625b45b277cab20ce24438f210
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-11 13:37:29 +0300

    Created a registration form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// GET   /registration 
+router.get('/register',function (req, res, next){
+  // next middle ware to do after the response
+ // return res.send('Registration today!');
+   return res.render('register', { title:'Sign Up'});
+})
+
+// POST /registration
+router.post('/register', function(req,res,next){
+   return res.send('User created');
+   
+});
+
 // GET /
 router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
